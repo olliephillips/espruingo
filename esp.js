f90b6e86c969587699ffedb70e90957a7ac99e3c
Ollie Phillips <ollie@eantics.co.uk>
2015-10-09 15:06:48 +0100

    Module loading plus example

--- esp.js
+++ esp.js
@@ -1,18 +1,20 @@
-// Sample script LED flashing script
-// Based on http://www.espruino.com/Quick+Start
+// Module loading example
+var s = require("servo").connect(C7);
+var Clock = require("clock").Clock;
 
+var clk=new Clock(2015,9,9,12,00,0,0);   // Initialise with specific date
+var servoPos = [0,0];
 
-var on = false;
-function toggle1() {
- on = !on;
- digitalWrite(LED1, on);
- 
+function getPositions() {
+	var t = getTime();
+	servoPos = [ 0.5+Math.sin(t)*0.5, 0.5+Math.cos(t)*0.5 ];
+	console.log(servoPos);
+	var d1=clk.getDate(); 
+   	console.log("Reading taken at: " + d1.toString());
 }
-function toggle2() {
- on = !on;
- digitalWrite(LED2, on);
+function moveServos() {
+	digitalPulse(A1,1,1+E.clip(servoPos[0],0,1));
+	digitalPulse(A2,1,1+E.clip(servoPos[1],0,1));
 }
-var i = setInterval(toggle1, 100);
-var i2 = setInterval(toggle2, 40);
-console.log("Disco..");
 
+setInterval("getPositions();moveServos()", 50);
--- espruingo.go
+++ espruingo.go
@@ -7,22 +7,33 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/daviddengcn/go-colortext"
 	"github.com/go-fsnotify/fsnotify"
 	"github.com/jacobsa/go-serial/serial"
+	"io"
 	//"github.com/tdewolff/minify"
 	//"github.com/tdewolff/minify/js"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
 
+var coreModules = []string{
+	"CC3000",
+	"ESP8266WiFi",
+	"http",
+	"WIZnet",
+}
+
 func main() {
 	var targetFile, device string
-
 	// Handle arguments
 	flag.Parse()
 	if len(flag.Args()) < 1 {
@@ -36,14 +47,14 @@ func main() {
 		device = flag.Args()[1]
 	}
 	targetFile = flag.Args()[0]
-
+	log.Println(device)
 	// Set up connection
 	options := serial.OpenOptions{
 		PortName:        device,
 		BaudRate:        19200,
 		DataBits:        8,
 		StopBits:        1,
-		MinimumReadSize: 4,
+		MinimumReadSize: 1,
 	}
 
 	s, err := serial.Open(options)
@@ -75,16 +86,36 @@ func main() {
 			case event := <-watcher.Events:
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					// File has changed, read it, send a reset() to Espurino and send the file line by line
-					_, err = s.Write([]byte("reset();\n"))
-
+					_, err = s.Write([]byte("\x03reset();\n"))
+					//\x03reset();\n
 					colorLog("< Writing to board", "blue")
 					// Open and scan file
-					file, err := os.Open(targetFile)
-					if err != nil {
-						log.Fatal(err)
-					}
-					defer file.Close()
 
+					fContents := bytes.NewBuffer(nil)
+
+					f, _ := os.Open(targetFile) // Error handling elided for brevity.
+					io.Copy(fContents, f)       // Error handling elided for brevity.
+					f.Close()
+
+					// This is our script
+					script := string(fContents.Bytes())
+
+					// Load modules
+					script = loadModules(script)
+
+					// Write to board
+					//log.Println(script)
+					//os.Exit(0)
+					script = "echo(0)\n" + script + "echo(1)\n"
+					_, err = s.Write([]byte(script))
+					/*
+
+						file, err := os.Open(targetFile)
+						if err != nil {
+							log.Fatal(err)
+						}
+						defer file.Close()
+					*/
 					/*
 						// Minify
 						// This the bones of it, how to make it work here, and should it be optional
@@ -94,17 +125,18 @@ func main() {
 							log.Fatal("js.Minify:", err)
 						}
 					*/
+					/*
+						scanner := bufio.NewScanner(file)
 
-					scanner := bufio.NewScanner(file)
+							for scanner.Scan() {
+								//Send each line to Espruino
+								_, err = s.Write([]byte(scanner.Text() + "\n"))
+							}
 
-					for scanner.Scan() {
-						//Send each line to Espruino
-						_, err = s.Write([]byte(scanner.Text() + "\n"))
-					}
-
-					if err := scanner.Err(); err != nil {
-						log.Fatal(err)
-					}
+							if err := scanner.Err(); err != nil {
+								log.Fatal(err)
+							}
+					*/
 				}
 			case err = <-watcher.Errors:
 				colorLog("! Unexpected error..", "red")
@@ -173,4 +205,53 @@ func colorLog(msg string, color string) {
 	}
 	fmt.Println(msg)
 	ct.ResetColor()
+}
+
+// This function takes care of the module loading and namespacing
+func loadModules(script string) string {
+	var moduleUri = "http://www.espruino.com/modules"
+	var moduleName string
+	var moduleJS string
+
+	// Scan script for require statements, first set up regex
+	r, _ := regexp.Compile("require\\(\"(.*)\"\\)")
+
+	// With our matches
+	for _, req := range r.FindAllString(script, -1) {
+		// For each match
+		moduleName = strings.Split(req, "\"")[1]
+		// If a core module we don't need to load it
+		if !contains(coreModules, moduleName) {
+			// Load the module
+			resp, err := http.Get(moduleUri + "/" + moduleName + ".min.js")
+			if err != nil {
+				colorLog("! Could not get a module:  "+moduleName, "red")
+				os.Exit(0)
+				log.Fatal(err)
+			}
+			defer resp.Body.Close()
+			contents, err := ioutil.ReadAll(resp.Body)
+			moduleJS = "var espruingo_" + moduleName + " = {};\n" + string(contents)
+			// We need to make some changes the module and our script to namespace and use it
+			// Replace the "exports." piece with "var espruingo.moduleName."
+			moduleJS = strings.Replace(moduleJS, "exports.", "espruingo_"+moduleName+".", 1)
+			// Replace the require("moduleName") with  "espruingo.moduleName"
+			script = strings.Replace(script, "require(\""+moduleName+"\")", "espruingo_"+moduleName, 1)
+
+			// We need to add the module to the top of our script
+			script = moduleJS + "\n\n" + script
+		}
+
+	}
+	return script
+}
+
+// Helper contains function to see if in slice
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
